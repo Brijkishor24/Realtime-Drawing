8f4abb136113229c0d8f3254dabf13f81516b30a
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2020-12-15 15:26:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 noseX=0;
 noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
 function setup(){
-    canvas=createCanvas(600,400);
+    canvas=createCanvas(600,600);
     canvas.position(550,200);
     video=createCapture(VIDEO);
     video.size(500,400);
@@ -17,8 +20,17 @@ function getPoses(results){
         console.log(results);
        noseX=results[0].pose.nose.x;
         noseY=results[0].pose.nose.y;
+      console.log("Nose X="+noseX+","+"Nose Y="+noseY);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX - rightWristX);
     }
 }
 function draw(){
     background("#00FF00");
+
+    document.getElementById("sq-length").innerHTML="Width and Height of the Square = "+difference+"px";
+    fill("white");
+    stroke("white");
+    square(noseX,noseY,difference);
 }
